8100dcff0d305af489f141806006b9ca515a0b02
ydullii <maximilianzitzmann3@gmail.com>
2023-03-09 20:56:08 +0100

    added Instance to Main Class

--- Varo Plugin/src/main/java/dullimc/varoplugin/Main.java
+++ Varo Plugin/src/main/java/dullimc/varoplugin/Main.java
@@ -4,6 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    public static Main instance;
+    public static dullimc.varoplugin.Main getInstance() {return instance;}
+
     @Override
     public void onEnable() {
         // Plugin startup logic
